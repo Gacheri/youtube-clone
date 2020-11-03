f5644f9b7c511a5b88cc72500ea09a45b75d006c
Gacheri <michenibrenda81@gmail.com>
2020-11-03 14:26:37 +0300

    first commit

---
+++ frontend/views/layouts/main.php
@@ -0,0 +1,21 @@
+<?php
+
+/* @var $this \yii\web\View */
+
+/* @var $content string */
+
+use common\widgets\Alert;
+
+$this->beginContent('@frontend/views/layouts/base.php');
+?>
+<?php echo $this->render('_header') ?>
+<main class="d-flex">
+
+    <?php echo $this->render('_sidebar') ?>
+
+    <div class="content-wrapper p-5">
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</main>
+<?php $this->endContent() ?>
